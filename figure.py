d693c8fa61d20a26d9e28e645b930a39592069bf
hoonjong.baek <hoonjong.baek@quantil.com>
2023-06-01 23:15:44 +0900

    implement hm5

--- figure.py
+++ figure.py
@@ -1,69 +1,79 @@
-## 오픈소스와 파이썬 Homework#4
-
+## 오픈소스와 파이썬 Homework#5
 """
 figure 모듈은 그림과 관련된 함수 및 클래스를 제공하는 모듈입니다.
 line class를 이용하여 선의 길이를 설정하고 참조하는 것을 수행하며
-area_square, area_circle, area_regular_triangle 함수로
-각각 정사각형, 원, 정삼각형의 넓이를 반환합니다.
+area_rectangle, area_ellipse, area_right_triangle 함수로
+각각 직사각형, 타원, 직각삼각형의 넓이를 반환합니다.
 """
 class line:
     """"
-    line 클래스는 선의 길이에 대해 저장하고 있는 클래스입니다.
-    변수로는 외부에서 접근 불가능한 __length가 있으며,
+    line 클래스는 가로와 세로의 길이에 대해 저장하고 있는 클래스입니다.
+    변수로는 외부에서 접근 불가능한 __width와 __height가 있으며,
     해당 변수를 수정하고 접근하기 위한 메소드로는 
     set_length와 get_length가 있습니다.
     """
-    __length = 0
+    __width = 0
+    __height = 0
 
-    def __init__(self, initial_value):
+    def __init__(self, initial_width, initial_height):
         """
-        생성자 초기 length 값을 받습니다.
+        생성자 초기 width 값과 height 값을 받습니다.
         Args:
-            initial_value (int or float): 초기 선의 길이 값입니다.
+            initial_width (int or float): 초기 가로의 길이 값입니다.
+            initial_height (int or float): 초기 세로의 길이 값입니다.
         """
-        self.__length = initial_value
+        self.__width = initial_width
+        self.__height = initial_height
 
-    def set_length(self, length):
-        self.__length = length
+    def set_length(self, width, height):
+        self.__width = width
+        self.__height = height
         """
-        선의 길이를 수정합니다.
+        가로와 세로의 길이를 수정합니다.
         Args:
-            length (int or float): 수정하고자 하는 선의 길이 값입니다.
+            width (int or float): 수정하고자 하는 가로의 길이 값입니다.
+            height (int or float): 수정하고자 하는 세로의 길이 값입니다.
         """
     def get_length(self):
         """
-        객체가 저장하고 있는 선의 길이를 반환합니다.
+        객체가 저장하고 있는 가로와 세로의 길이를 반환합니다.
         Returns:
-            int or float: 저장하고 있는 선의 길이 값입니다.
+            int or float: 저장하고 있는 가로와 세로의 길이 값입니다.
         """
-        return self.__length
+        return self.__width, self.__height
 
 import math
 
-def area_square(length):
+def area_rectangle(width, height):
     """
-    area_square 함수는 길이를 입력받아 정사각형의 넓이를 구하는 함수입니다.
+    area_rectangle 함수는 밑변과 높이의 길이를 입력받아 직사각형의 넓이를 구하는 함수입니다.
     Args:
-        length (int or float): 한 변의 길이입니다.
+        width (int or float): 밑변의 길이입니다.
+        height (int or float): 높이의 길이입니다.
     Returns:
-        int or float: 정사각형의 넓이를 반환합니다.
+        int or float: 직사각형의 넓이를 반환합니다.
     """
-    return length * length
-def area_circle(length):
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height
+def area_ellipse(width, height):
     """
-    area_circle 함수는 길이를 입력받아 원의 넓이를 구하는 함수입니다.
+    area_ellipse 함수는 짧은쪽과 긴쪽 반지름의 길이를 입력받아 타원의 넓이를 구하는 함수입니다.
     Args:
-        length (int or float): 반지름의 길이입니다.
+        width (int or float): 짧은쪽 반지름의 길이입니다.
+        height (int or float): 긴쪽 반지름의 길이입니다.
     Returns:
-        int or float: 원의 넓이를 반환합니다.
+        int or float: 타원의 넓이를 반환합니다.
     """
-    return length * length * math.pi
-def area_regular_triangle(length):
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height * math.pi
+def area_right_triangle(width, height):
     """"
-    area_regular_triangle 함수는 길이를 입력받아 정삼각형의 넓이를 구하는 함수입니다.
+    area_right_triangle 함수는 밑변과 세로의 길이를 입력받아 직각삼각형의 넓이를 구하는 함수입니다.
     Args:
-        length (int or float): 한 변의 길이입니다.
+        width (int or float): 밑변의 길이입니다.
+        height (int or float): 높이의 길이입니다.
     Returns:
-        int or float: 정삼각형의 넓이를 반환합니다.
+        int or float: 직각삼각형의 넓이를 반환합니다.
     """
-    return length * length * math.sqrt(3) / 4
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height / 2
--- figure_main.py
+++ figure_main.py
@@ -1,18 +1,30 @@
 # %%
 
-# 모듈 figure를 불러와 정사각형, 정삼각형, 원의 넓이를 계산하는 프로그램입니다.
+# 모듈 figure를 불러와 직사각형, 직각삼각형, 타원의 넓이를 계산하는 프로그램입니다.
 
 import figure
 
-myline = figure.line(10)
+myline = figure.line(10, 20)
 
-square = figure.area_square(myline.get_length())
-print(square)
+width, height = myline.get_length()
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("please input positive number for width and height")
 
-myline.set_length(20)
-regular_triangle = figure.area_regular_triangle(myline.get_length())
-print(regular_triangle)
+myline.set_length(10, 30)
+width, height = myline.get_length()
+try:
+    right_triangle = figure.area_right_triangle(width, height)
+    print(right_triangle)
+except ValueError:
+    print("please input positive number for width and height")
 
-myline.set_length(30)
-circle = figure.area_circle(myline.get_length())
-print(circle)
+myline.set_length(20, 10)
+width, height = myline.get_length()
+try:
+    ellipse = figure.area_ellipse(width, height)
+    print(ellipse)
+except ValueError:
+    print("please input positive number for width and height")
